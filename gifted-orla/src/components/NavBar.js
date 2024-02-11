477679669b662446fdb51e8620c01b2b8663b85e
Thomas <sorvad@gmail.com>
2024-02-11 12:35:12 +0000

    Changed icons on navbar to home, search an library

--- gifted-orla/src/components/NavBar.js
+++ gifted-orla/src/components/NavBar.js
@@ -1,10 +1,9 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 import { Paper } from "@mui/material";
 
 export default function NavBar() {
@@ -18,9 +17,9 @@ export default function NavBar() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+        <BottomNavigationAction label="Library" icon={<LibraryBooksIcon />} />
       </BottomNavigation>
     </Paper>
   );
