cb4e2d870dc084a81bbd821ae35e7d06d5c6e64d
Thomas <sorvad@gmail.com>
2024-02-15 16:03:18 +0000

    Removed twitter and facebook login for now

--- gifted-orla/src/components/AuthUI.js
+++ gifted-orla/src/components/AuthUI.js
@@ -1,25 +1,21 @@
 import { auth } from "./AuthProvider";
 import "firebaseui/dist/firebaseui.css";
 import * as firebaseui from "firebaseui";
-import {
-  GoogleAuthProvider,
-  FacebookAuthProvider,
-  TwitterAuthProvider,
-  EmailAuthProvider,
-} from "firebase/auth";
+import { GoogleAuthProvider, EmailAuthProvider } from "firebase/auth";
 
 export const uiConfig = {
+  // TODO sign in URL
   signInSuccessUrl: "<url-to-redirect-to-on-success>",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     GoogleAuthProvider.PROVIDER_ID,
-    FacebookAuthProvider.PROVIDER_ID,
-    TwitterAuthProvider.PROVIDER_ID,
+    // TODO FacebookAuthProvider.PROVIDER_ID,
+    // TODO: TwitterAuthProvider.PROVIDER_ID,
     EmailAuthProvider.PROVIDER_ID,
   ],
   // tosUrl and privacyPolicyUrl accept either url string or a callback
   // function.
-  // Terms of service url/callback.
+  // TODO Terms of service url/callback.
   signInFlow: "popup",
   tosUrl: "<your-tos-url>",
   // Privacy policy url/callback.
