e48a44f9819d5c8698b6cfc6fc4b76ca4a3e026b
rnflt thr <sorvad@gmail.com>
2024-03-06 17:21:03 +0000

    Refactored FetchData

--- gifted-orla/src/service/FetchData.js
+++ gifted-orla/src/service/FetchData.js
@@ -1,15 +1,17 @@
 import { getDocs, collection } from "firebase/firestore";
-
 import { db } from "./firebase";
 
 const fetchData = async (collectionName, setConst) => {
-  await getDocs(collection(db, collectionName)).then((Snapshot) => {
-    const data = Snapshot.docs.map((doc) => ({
+  try {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    const data = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
     setConst(data);
-  });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 export default fetchData;
