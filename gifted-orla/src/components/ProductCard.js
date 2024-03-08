6a047873362d7fed1d296eb679f5116a7cf97c20
rnflt thr <sorvad@gmail.com>
2024-03-08 15:32:29 +0000

    Rewrote productcard as function

--- gifted-orla/src/components/ProductCard.js
+++ gifted-orla/src/components/ProductCard.js
@@ -1,50 +1,43 @@
-import * as React from "react";
-
+import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-
+import IconButton from "@mui/material/IconButton";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-
 import DropdownLists from "./DropdownLists";
 
-class ProductCard extends React.Component {
-  render() {
-    return (
-      <Card sx={{ display: "flex", flexDirection: "row" }}>
-        <React.Fragment>
-          <CardActionArea
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-            }}
-          >
-            <CardMedia
-              component="img"
-              sx={{ width: "150px" }}
-              image={require("../assets/house.jpg")}
-              alt="green iguana"
-            />
-            <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography variant="h5">{this.props.name}</Typography>
-              <Typography variant="subtitle1" color="text.secondary">
-                {this.props.brand}
-              </Typography>
-              <Typography>{this.props.price}</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions sx={{ display: "flex", flexDirection: "column" }}>
-            <DropdownLists product={this.props} />
-            <IconButton><MoreHorizIcon /></IconButton>
-          </CardActions>
-        </React.Fragment>
-      </Card>
-    );
-  }
-}
+const ProductCard = (props) => {
+  return (
+    <Card sx={{ display: "flex", flexDirection: "row" }}>
+      <CardActionArea
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{ width: "150px" }}
+          image={require("../assets/house.jpg")}
+          alt="green iguana"
+        />
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography variant="h5">{props.name}</Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {props.brand}
+          </Typography>
+          <Typography>{props.price}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions sx={{ display: "flex", flexDirection: "column" }}>
+        <DropdownLists product={props} />
+        <IconButton><MoreHorizIcon /></IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default ProductCard;
