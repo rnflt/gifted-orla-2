011c2ece831cfa82479c5d06c4662a520e487f21
rnflt thr <sorvad@gmail.com>
2024-03-08 15:34:19 +0000

    Refactored productList as function

--- gifted-orla/src/components/ProductList.js
+++ gifted-orla/src/components/ProductList.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import ProductCard from "./ProductCard";
 
-class ProductList extends Component {
-  render() {
-    const products = this.props.products;
-    return products.map((product) => (
-      <ProductCard
-        key={product.Key}
-        brand={product.Brand}
-        name={product.Name}
-        price={product.Price}
-        lists={product.lists}
-        id={product.id}
-      />
-    ));
-  }
-}
+const ProductList = ({ products }) => {
+  return products.map((product) => (
+    <ProductCard
+      key={product.Key}
+      brand={product.Brand}
+      name={product.Name}
+      price={product.Price}
+      lists={product.lists}
+      id={product.id}
+    />
+  ));
+};
+
 export default ProductList;
